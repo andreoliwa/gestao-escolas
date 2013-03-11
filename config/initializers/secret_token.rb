203a3f05cde25a29ab88e0abde28fe1802d13ec4
Wagner Augusto Andreoli <wagnerandreoli@gmail.com>
2013-03-11 11:38:34 -0300

    [#45685001] Rails installation without default unit tests and before bundle. rails new . --skip-test-unit --skip-bundle

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GestaoEscolas::Application.config.secret_token = '4a913c13b97be0bcb369370b9b4c2c2e965cf9f4b4510195422eed41962bddd00215dc2552a1ed7556c7313ce7f36f1c7b86db83b42c2a7ef87582f5e2416cb4'
